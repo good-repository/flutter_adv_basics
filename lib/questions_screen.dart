205ba13e0a4e9502e18c5c4cc2746a70aa1fcd9a
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-10-22 22:00:01 -0300

    Display dynamic questions and answers
    Updated QuestionsScreen to import questions data and display the first question's text and its answers dynamically. Refactored AnswerButton imports and replaced hardcoded answers with a map over the current question's answers.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
-import 'answer_button.dart';
+import 'package:adv_basics/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,20 +14,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Question',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(fontSize: 16, color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(onTap: () {}, answerText: 'Answer 1'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 2'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 3'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 4'),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {
+                // Handle answer selection
+              },
+            );
+          }),
         ],
       ),
     );
