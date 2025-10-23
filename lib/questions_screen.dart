e66bf905510ecb0eac82750c2c93d0767707ec31
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-10-22 22:41:59 -0300

    Add Google Fonts and update text styles
    Integrated the google_fonts package and applied Lato font styles to key text elements in questions_screen.dart and start_screen.dart for improved UI appearance. Updated pubspec.yaml to include google_fonts dependency.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:adv_basics/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:flutter/material.dart';
 import 'package:adv_basics/answer_button.dart';
 
@@ -34,7 +36,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                color: Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
