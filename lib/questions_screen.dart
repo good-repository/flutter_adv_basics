857e8753fa66214b184da775ffe71ed866a0bf60
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2025-10-22 22:34:40 -0300

    Add question navigation to QuestionsScreen
    Introduces state management for the current question index and updates the UI to display the next question when an answer is selected.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -31,12 +39,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
             const SizedBox(height: 30),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(
-                answerText: answer,
-                onTap: () {
-                  // Handle answer selection
-                },
-              );
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
             }),
           ],
         ),
